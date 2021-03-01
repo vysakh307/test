97f57d86e8391f76b61094e3f554ed5cfce37c0f
vysakh307 <78088834+vysakh307@users.noreply.github.com>
2021-03-02 00:22:37 +0530

    test removed

--- phykon_custom/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import applicant_assessment
-from . import models, hr_employee, hr_job, hr_applicant, survey, hr_holidays, hr_attendance, hr_employee_contract, hr_generate_shift, calendar, hr_exit
-from . import resource_mixin
--- phykon_custom/models/calendar.py
+++
@@ -1,9 +0,0 @@
-from odoo import fields, models, api, _
-from odoo.exceptions import ValidationError, Warning
-
-
-# URL field for calendar
-class CustomCalendar(models.Model):
-    _inherit = 'calendar.event'
-
-    url = fields.Char(string="Virtual Location Link")
